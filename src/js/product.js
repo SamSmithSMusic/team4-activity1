d841d33985714832a812dd123fe4d33f72f7c301
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2025-01-25 17:59:53 -0700

    Created cart animation

--- src/js/product.js
+++ src/js/product.js
@@ -4,3 +4,9 @@ import {getParam} from "./utils.mjs";
 const productId = getParam("product");
 
 loadProductDetails(productId)
+
+let btn = document.querySelector("#addToCart");
+
+btn.addEventListener("click", () => {
+    btn.classList.toggle("clicked")
+})
